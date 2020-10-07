490bc204c0ab1f805c69d9723e08f72d26fac971
profh <profh@cmu.edu>
2020-10-07 18:33:46 -0400

    Very basic concurrency demo

--- ConcurrencyDemo/ViewController.swift
+++ ConcurrencyDemo/ViewController.swift
@@ -1,20 +1,56 @@
-//
 //  ViewController.swift
 //  ConcurrencyDemo
-//
-//  Created by Larry Heimann on 10/7/20.
-//  Copyright © 2020 Larry Heimann. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var primeNumberButton: UIButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+  
+  @IBAction func calculatePrimeNumbers(_ sender: Any) {
+    // calculatePrimes()
+    
+    // DispatchQueue.main.async {
+    //   self.calculatePrimes()
+    // }
+    
+    DispatchQueue.global(qos: .default).async {
+      self.calculatePrimes()
+    }
 
+  }
+  
+  // MARK: Calculating Primes
+  // These methods are just so we can spend a lot of time tying up the interface
+  func isPrime(number: Int) -> Bool {
+    if number <= 1 {
+      return false
+    }
+    if number <= 3 {
+      return true
+    }
+    var i = 2
+    while i * i <= number {
+      if number % i == 0 {
+        return false
+      }
+      i = i + 2
+    }
+    return true
+  }
+  
+  func calculatePrimes() {
+    for number in 0...100_000_000 {
+      let isPrimeNumber = isPrime(number: number)
+      print("\(number) is prime: \(isPrimeNumber)")
+    }
+  }
 
 }
 
